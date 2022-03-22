411dd3b40fc14c3e28add0bda2e35b355f1e45a4
KenanP996 <kenan.piljevic@stu.ibu.edu.ba>
2022-03-23 00:59:26 +0100

    main

--- index.php
+++ index.php
@@ -11,7 +11,15 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected Successfully";
+
+    $stmt = $conn->prepare("Select * from 'todo'");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($result);
+
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+
 ?>
