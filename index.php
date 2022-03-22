db7a7cca269740a075f94be30c1ccdf44c0d7bbf
KenanP996 <kenan.piljevic@stu.ibu.edu.ba>
2022-03-22 23:52:45 +0100

    main

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'root';
-$db_db = 'Image_Sharing_Platform';
+  $db_host = 'localhost';
+  $db_user = 'root';
+  $db_password = 'root';
+  $db_db = 'Image_Sharing_Platform';
  
-$mysqli = @new mysqli(
-$db_host,
-$db_user,
-$db_password,
-$db_db
-);
+  $mysqli = @new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+  );
 	
-if ($mysqli->connect_error) {
+  if ($mysqli->connect_error) {
     echo 'Errno: '.$mysqli->connect_errno;
     echo '<br>';
     echo 'Error: '.$mysqli->connect_error;
     exit();
-}
+  }
 
-echo 'Success: A proper connection to MySQL was made.';
-echo '<br>';
-echo 'Host information: '.$mysqli->host_info;
-echo '<br>';
-echo 'Protocol version: '.$mysqli->protocol_version;
+  echo 'Success: A proper connection to MySQL was made.';
+  echo '<br>';
+  echo 'Host information: '.$mysqli->host_info;
+  echo '<br>';
+  echo 'Protocol version: '.$mysqli->protocol_version;
 
-$mysqli->close();
-
-echo "Hello World";
+  $mysqli->close();
 ?>
